59d387e47a97e52e1d3bea6f068a0bb2b56b0516
lucas.tk <lucas.tk@taehyungui-MacBookPro.local>
2023-08-21 16:04:53 +0900

    leetcode 344. Reverse String

---
+++ leetcode/344_reverse_string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        ans = s.reverse()
+
+        return ans
